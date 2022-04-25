13e6159a16bca48877cee8c031e2e98246c66625
Aysel9 <89453868+Aysel9@users.noreply.github.com>
2022-04-25 13:14:05 +0300

    Add files via upload
    ss

---
+++ components/FooterLandingPage.jsx
@@ -0,0 +1,15 @@
+const FooterLandingPage = () => {
+	return(
+		<div className="FooterLanding">
+			<div className="hr"></div>
+			<div className="general-footer">
+				<div className="img-privacy">
+					<img src="https://i.ibb.co/Pr5pF0M/Lagrange-logo-light.png" />
+					<div className="privacy-policy">Privacy Policy | Terms of use</div>
+				</div>
+				<div className="footer-address">4517 Washington Ave. Manchester, Kentucky 39495</div>
+			</div>
+		</div>
+		)
+}
+export default FooterLandingPage;
